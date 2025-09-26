7a8d83b77b44a52e35e871688fd52faed3ff6955
hlh2518 <39757859+hlh2518@users.noreply.github.com>
2025-09-26 17:28:52 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &[""];
-pub const PUBLIC_RS_PUB_KEY: &str = "4p9LCwNvCULIdTUe5v143aBiron+fYexyN3Pp2N2b5I=";
+pub const PUBLIC_RS_PUB_KEY: &str = "";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
